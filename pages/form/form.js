fc666c49cf12b0b829ded58eea4ddda7e9102e6b
wanshuxiao <wanshuxiao@gmail.com>
2017-12-01 16:00:29 +0800

    Revert "Revert "Initial commit""
    This reverts commit 80be4cbcfd184db7508aec9932f0fa9ed027b3e0.

---
+++ pages/form/form.js
@@ -0,0 +1,74 @@
+// form.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    serialNumber:'',
+    model:'',
+    rackPostion:'',
+    detail:''
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.setData({
+      serialNumber: options.serialNumber,
+      model: options.model,
+      rackPostion: options.rackPostion
+    })
+    // console.log(model)
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
---
+++ pages/index/index.js
@@ -0,0 +1,46 @@
+//index.js
+//获取应用实例
+var app = getApp()
+Page({
+  data: {
+    motto: 'Copyleft 上海电信版权所有',
+    userInfo: {},
+    //设备序列号在这个页面不用记录的，后面要删除
+    serialNumber: ''
+  },
+  //事件处理函数
+  // bindViewTap: function() {
+  //   wx.navigateTo({
+  //     url: '../logs/logs'
+  //   })
+  // },
+  onLoad: function () {
+    console.log('onLoad')
+    var that = this
+    //调用应用实例的方法获取全局数据
+    app.getUserInfo(function(userInfo){
+      //更新数据
+      that.setData({
+        userInfo:userInfo
+      })
+    })
+  },
+  scanCode: function () {
+    var that = this
+    var urlData = ''
+    wx.scanCode({
+      success: function (res) {
+        that.setData({
+          serialNumber: res.result
+        })
+        //向服务器发起一个request，上传serialNumber，返回model和rackPostion
+        urlData = '/pages/form/form?serialNumber='+that.data.serialNumber+'&model=DL380&rackPostion=周家渡K6'
+        wx.navigateTo({
+          url: urlData
+        })
+      },
+      fail: function (res) {
+      }
+    })
+  }
+})
